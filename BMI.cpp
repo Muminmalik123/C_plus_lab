81d1a29787f2ddfe681b1e42c4acbfc59f061b1a
Mehran Parvaiz <106099427+Iammehran72@users.noreply.github.com>
2022-10-31 19:59:01 +0530

    Add files via upload

---
+++ BMI.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double h,w;
+	cout<<"Enter Your Height(m) and Weight(kg):";
+	cin>>h>>w;
+	double	BMI=w/(h*h);
+	cout<<"Your BMI is : "<<BMI<<endl;
+	if(BMI<=18.5)
+	{
+		cout<<"You Are Underweight.";
+		
+	}
+	else if (BMI>=18.5 &&  BMI<=24.9 )
+	{
+		cout<<"You Are Normal.";
+		
+	}
+	else if (BMI>=25 && BMI<=29.9)
+	{
+		cout<<"You Are Overweight.";
+	}
+	else if (BMI>=30)
+	{
+		cout<<"You Are Obese.";
+	}
+}

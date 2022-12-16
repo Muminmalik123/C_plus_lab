8790f8f607ac6e9f4ff7df84b4375c5d16d45210
Mehran Parvaiz <106099427+Iammehran72@users.noreply.github.com>
2022-12-16 23:21:47 +0530

    Add files via upload

---
+++ CTYPHheader.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<CTYPE.H>
+using namespace std;
+int main(){
+    char character;
+    cout<<"Enter any character : ";
+    cin>>character;
+    cout<<islower(character)<<endl;
+
+return 0;
+}

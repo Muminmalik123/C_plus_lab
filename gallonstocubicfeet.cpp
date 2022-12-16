35e5c1a49f60c726dcd1bf66199f171de89920d0
Mehran Parvaiz <106099427+Iammehran72@users.noreply.github.com>
2022-12-16 23:22:16 +0530

    Add files via upload

---
+++ gallonstocubicfeet.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double gallons;
+    cout<<"Enter the vlue in Gallons :";
+    cin>>gallons;
+    cout<<"The Value in Cubic Feet is = "<<gallons/7.481<<endl;
+
+return 0;
+}

196ad017cadabb78c5a519a30d559e6f4b93fda4
Mehran Parvaiz <106099427+Iammehran72@users.noreply.github.com>
2022-12-17 00:25:53 +0530

    Add files via upload

---
+++ fraction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d;
+    char temp;
+    cout<<"Enter first fraction : ";
+    cin>>a>>temp>>b;
+    cout<<"Enter second fraction :";
+    cin>>c>>temp>>d;
+    int num = (a*d)+(b*c);
+    int den = (b*d);
+    cout<<"The sum = "<<num<<"/"<<den;
+    
+    
+
+return 0;
+}

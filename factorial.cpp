2db8f8f746df17921dddeac6530d1da090998f5d
Mehran Parvaiz <106099427+Iammehran72@users.noreply.github.com>
2023-02-20 19:21:44 +0530

    Add files via upload

--- factorial.cpp
+++ factorial.cpp
@@ -1,15 +1,30 @@
 #include<iostream>
 using namespace std;
+class factorial{
+	private:
+		int x;
+		
+	public:
+		void input()
+		{
+			cout<<"Enter any positive integer : ";
+			cin>>x;
+		}
+		void calculate()
+		{
+			int factorial =1;
+			for(int i=2; i<=x; i++){
+				factorial *=i;
+			} 
+			cout<<"Factorial = "<<factorial;
+		}
+
+};
 int main()
 {
-	int i, j=1;
-	int number;
-	cout<<"Enter a number : ";
-	cin>>number;
-	  for(i=1; i<=number; i++)
-       {
-       	j=j*i;
-	   }
-	   cout<<"Factorial of given number is : "<<j;
-	   return 0; 
+	factorial f;
+	f.input();
+	f.calculate();
+	return 0;
 }
+
